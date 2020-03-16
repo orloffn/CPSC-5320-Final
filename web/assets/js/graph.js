b82ffe75f8832682d549ba3b24985390f198d8b2
orloff <n.orloff@f5.com>
2020-03-16 15:43:20 -0700

    work on visualizations

--- web/assets/js/graph.js
+++ web/assets/js/graph.js
@@ -1,3 +1,84 @@
-Plotly.d3.csv('assets/js/data.csv', function(err, rows){
+function unpack(rows, key) {
+    return rows.map(function(row) { return row[key]; });
+}
 
+Plotly.d3.csv('assets/js/timeline.csv', function(err, rows){
+    var data = [{
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Confirmed cases',
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'confirmed')
+    },{
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Interest in "Mask"',
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'mask')
+    },{
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Interest in "Quarantine"',
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'quarantine')
+    },{
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Interest in "Symptom"',
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'symptom')
+    },{
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Interest in "Donald Trump"',
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'trump')
+    }];
+
+    Plotly.newPlot("graph1", data);
 });
+
+Plotly.d3.csv('assets/js/map.csv', function(err, rows){
+    var data = [{
+        type: 'scattergeo',
+        mode: 'markers',
+        locations: unpack(rows, 'country'),
+        marker: {
+            size: unpack(rows, 'normalized confirmed'),
+            color: unpack(rows, 'symptom'),
+            colorscale: 'Greens',
+            colorbar: {
+                title: 'Interest in "Symptom"'
+            },
+            line: {
+                color: 'black'
+            }
+        }
+    }];
+
+    var layout = {
+        title: '',
+        geo: {
+            projection: {
+                type: 'mercator'
+            },
+            showland: true,
+            landcolor: 'rgb(217, 217, 217)',
+            subunitwidth: 1,
+            countrywidth: 1,
+            subunitcolor: 'rgb(255,255,255)',
+            countrycolor: 'rgb(255,255,255)'
+        },
+        margin: {
+            l: 30,
+            r: 30,
+            b: 30,
+            t: 30,
+            pad: 4
+        }
+    };
+
+    Plotly.newPlot("graph2", data, layout, {showLink: false});
+});
+
+
