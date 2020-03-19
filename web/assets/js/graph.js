10aa8fba2989dcd363f53aa63e76138553820b3e
orloff <n.orloff@f5.com>
2020-03-18 20:19:21 -0700

    final fixes

--- web/assets/js/graph.js
+++ web/assets/js/graph.js
@@ -2,6 +2,10 @@ function unpack(rows, key) {
     return rows.map(function(row) { return row[key]; });
 }
 
+function logify(rows){
+    return rows.map(function(x) {return Math.log(x) * 10})
+}
+
 Plotly.d3.csv('assets/js/timeline.csv', function(err, rows){
     var data = [{
         type: 'scatter',
@@ -52,48 +56,49 @@ Plotly.d3.csv('assets/js/timeline.csv', function(err, rows){
     Plotly.newPlot("graph1", data, layout);
 });
 
-Plotly.d3.csv('assets/js/map.csv', function(err, rows){
-    var data = [{
-        type: 'scattergeo',
-        mode: 'markers',
-        text: unpack(rows, 'country'),
-        lat: unpack(rows, 'lat'),
-        lon: unpack(rows, 'long'),
-        marker: {
-            size: unpack(rows, 'normalized confirmed'),
-            color: unpack(rows, 'symptom'),
-            colorscale: 'Reds',
-            colorbar: {
-                title: 'Interest in "Symptom"'
-            },
-            line: {
-                color: 'black'
-            }
-        }
-    }];
+// Plotly.d3.csv('assets/js/map.csv', function(err, rows){
+//     console.log(rows)
+//     var data = [{
+//         type: 'scattergeo',
+//         mode: 'markers',
+//         text: unpack(rows, 'country'),
+//         lat: unpack(rows, 'lat'),
+//         lon: unpack(rows, 'long'),
+//         marker: {
+//             size: logify(unpack(rows, 'normalized confirmed')),
+//             color: unpack(rows, 'symptom'),
+//             colorscale: 'Reds',
+//             colorbar: {
+//                 title: 'Interest in "Symptom"'
+//             },
+//             line: {
+//                 color: 'black'
+//             }
+//         }
+//     }];
 
-    var layout = {
-        geo: {
-            projection: {
-                type: 'mercator'
-            },
-            showland: true,
-            landcolor: 'rgb(217, 217, 217)',
-            subunitwidth: 1,
-            countrywidth: 1,
-            subunitcolor: 'rgb(255,255,255)',
-            countrycolor: 'rgb(255,255,255)'
-        },
-        margin: {
-            l: 30,
-            r: 30,
-            b: 30,
-            t: 30,
-            pad: 4
-        }
-    };
+//     var layout = {
+//         geo: {
+//             projection: {
+//                 type: 'mercator'
+//             },
+//             showland: true,
+//             landcolor: 'rgb(217, 217, 217)',
+//             subunitwidth: 1,
+//             countrywidth: 1,
+//             subunitcolor: 'rgb(255,255,255)',
+//             countrycolor: 'rgb(255,255,255)'
+//         },
+//         margin: {
+//             l: 30,
+//             r: 30,
+//             b: 30,
+//             t: 30,
+//             pad: 4
+//         }
+//     };
 
-    Plotly.newPlot("graph2", data, layout, {showLink: false});
-});
+//     Plotly.newPlot("graph2", data, layout, {showLink: false});
+// });
 
 
